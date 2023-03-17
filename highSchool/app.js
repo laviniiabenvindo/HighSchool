21a64723abd9aeb66bda59025dde24e9777df90c
Lavinia Benvindo <110117509+laviniiabenvindo@users.noreply.github.com>
2023-03-17 17:30:52 -0300

    Add files via upload

--- highSchool/app.js
+++ highSchool/app.js
@@ -1,19 +1,23 @@
 const email = document.getElementById('email');
-const senha = document.getElementById('senha');
+const password = document.getElementById('password');
 const form = document.getElementById('form');
 const messageError = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
-    let message = [];
+    let messages = [];
     let regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{10,16}$/;
 
     if(email.value === '' || email.value === null){
         messages.push('O campo e-mail é obrigatório');
     }
-    if(senha.value.length <= 8){
+    if(password.value.length <= 8){
         messages.push('A senha tem que ter mais de 8 caracteres');
     }
-    if(!regex.test(senha.value)){
-        messeges.push('Precisa ter uma letra e maiúcula e minúscula, caractere especial e número');
+    if(!regex.test(password.value)){
+        messages.push('Precisa ter uma letra e maiúcula e minúscula, caractere especial e número');
+    }
+    if(messages.length > 0){
+        event.preventDefault();
+        messageError.innerHTML = messages.join(',');
     }
 });
